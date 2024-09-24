c7a4237837f7a54e4af02bf0283aaa41df62c137
Arctan <artan.bajqinca@gmail.com>
2024-09-24 12:35:33 +0200

    End-End

---
+++ BE/test/web/pages/login_page.py
@@ -0,0 +1,14 @@
+from tests.web.pages.page_base import PageBase
+from tests.web.helpers.element import Element
+from munch import munchify
+
+
+class LoginPage(PageBase):
+    def __init__(self, driver):
+        PageBase.__init__(self, driver = driver)
+
+        self.page_elements = {
+            'username': Element('//input[@id="username"]', self),
+        }
+
+        self.elements = munchify(self.page_elements)
---
+++ BE/test/web/web_test.py
@@ -0,0 +1,8 @@
+import pytest
+from tests.web.test_base import WebBase
+from tests.web.pages.login_page import LoginPage
+
+class TestWeb(WebBase):
+
+    def test_login(self):
+        pass
